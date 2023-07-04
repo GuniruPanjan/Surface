784c19da92f5c861fb16c9ad78d52a978f48bafb
GuniruPanjan <135081686+GuniruPanjan@users.noreply.github.com>
2023-07-04 10:25:55 +0900

    Csya-pukadai4

---
+++ Csya-pukadai4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Csya_pukadai4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
