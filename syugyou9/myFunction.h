55401b5dde1b2679de141bfc80d657378f98b7d8
GuniruPanjan <135081686+GuniruPanjan@users.noreply.github.com>
2023-07-04 10:47:25 +0900

    zyugyou9

---
+++ syugyou9/myFunction.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+//プロトタイプ宣言
+void f(int x);   //引数xの値を表示する
+void f2(int x);    //引数xの値を表示する
+void f3(int x, int y);     //引数xの値を表示する
+
---
+++ syugyou9/zyugyou9.1.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include"myFunction.h"
+
+int main(void)
+{
+	//ì¬‚µ‚½ŠÖ”‚ğŒÄ‚Ño‚µA
+	//•\¦‚µ‚½*‚Ì”‚ğprintf•\¦‚·‚é
+
+	return 0;
+}
+/*
+int main(void)
+{
+	f(2);
+	f2(15);
+	return 0;
+}
+*/
+
+
+/*
+int main(void)
+{
+	char inputChar = 'A';
+
+	printf("”¼Šp‚P•¶š‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢\n");
+
+	inputChar = getchar();
+
+	printf("”¼Šp‚P•¶š = %c\n", inputChar);
+
+	int inputNum = 0;
+
+	printf("®”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢\n");
+
+	scanf_s("%d", &inputNum);
+
+	printf("®” = %d\n", inputNum);
+
+	float inputFloat = 0.0f;
+
+	printf("­”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢\n");
+
+	scanf_s("%f", &inputFloat);
+
+	printf("­” = %f\n", inputFloat);
+
+
+	return 0;
+}
+*/
