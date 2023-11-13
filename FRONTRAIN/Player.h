683ded3919ef17af273fcd259be676a258776162
GuniruPanjan <135081686+GuniruPanjan@users.noreply.github.com>
2023-11-13 20:50:37 +0900

    up

---
+++ FRONTRAIN/Player.h
@@ -0,0 +1,22 @@
+#pragma once
+class Player
+{
+public:
+	Player();
+	~Player();
+
+	void Init();
+	void Update();
+	void Draw();
+
+private:
+	int HP;
+	int Speed;
+	//プレイヤーの画像を入れる変数
+	int playerGraph;
+
+	//プレイヤーの初期位置
+	int PlayerX;
+	int PlayerY;
+};
+
