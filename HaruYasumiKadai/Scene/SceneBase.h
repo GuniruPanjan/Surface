c21908352b9199af5c903be46b6d783a45bafa28
GuniruPanjan <135081686+GuniruPanjan@users.noreply.github.com>
2024-04-15 18:43:26 +0900

    up

--- HaruYasumiKadai/Scene/SceneBase.h
+++ HaruYasumiKadai/Scene/SceneBase.h
@@ -1,5 +1,8 @@
 #pragma once
 #include<memory>
+#include "Player/Player.h"
+#include "Map/BlackHole.h"
+#include "Map/Map.h"
 
 class SceneBase : public std::enable_shared_from_this<SceneBase>
 {
@@ -14,6 +17,18 @@ public:
 	//シーン変更しない場合は自身のポインタ
 	virtual void Draw() = 0;  //毎フレーム行う描画処理(純粋仮想関数にする)
 	virtual void End() = 0; //シーンを抜ける時の終了処理(純粋仮想関数にする)
+
+	//Playerを管理するポインタ
+//std::shared_ptr<Player> player = std::make_shared<Player>();
+	Player Play;
+
+	//BlackHoleを管理するポインタ
+	//std::shared_ptr<BlackHole> blackhole = std::make_shared<BlackHole>();
+	BlackHole blackhole;
+
+	//マップを管理するポインタ
+	//std::shared_ptr<Map> map = std::make_shared<Map>();
+	Map map;
 
 	//フェードさせられる変数
 	int FeadColor = 0;
--- HaruYasumiKadai/Scene/SceneManager.h
+++ HaruYasumiKadai/Scene/SceneManager.h
@@ -1,20 +1,8 @@
 #pragma once
-#include "Player/Player.h"
-#include "Map/BlackHole.h"
-#include "Map/Map.h"
+
 #include<memory>
 
-//Playerを管理するポインタ
-//std::shared_ptr<Player> player = std::make_shared<Player>();
-Player Play;
 
-//BlackHoleを管理するポインタ
-//std::shared_ptr<BlackHole> blackhole = std::make_shared<BlackHole>();
-BlackHole blackhole;
-
-//マップを管理するポインタ
-//std::shared_ptr<Map> map = std::make_shared<Map>();
-Map map;
 
 class SceneBase;
 
